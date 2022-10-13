b1b64c9ac1cfdf3a56bb3c6550d44052fd4be2fd
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2022-10-13 16:42:24 +0200

    feat: sig. and proc. for PC, CNT, PTR & MX

--- cpu.vhd
+++ cpu.vhd
@@ -49,21 +49,92 @@ ARCHITECTURE behavioral OF cpu IS
   SIGNAL PTR_DEC    : STD_LOGIC;
   -- CNT (counter for loops)
   SIGNAL CNT        : STD_LOGIC_VECTOR(7 DOWNTO 0);
-  SIGNAL CNT_ONE    : STD_LOGIC;
   SIGNAL CNT_INC    : STD_LOGIC;
   SIGNAL CNT_DEC    : STD_LOGIC;
   -- Helper signals
-  SIGNAL MX1_sel    : STD_LOGIC;
-  SIGNAL MX2_sel    : STD_LOGIC;
+  SIGNAL MX1_SEL    : STD_LOGIC;
+  SIGNAL MX2_SEL    : STD_LOGIC_VECTOR(1 DOWNTO 0);
+  SIGNAL CNT_ZERO   : STD_LOGIC;
   SIGNAL RDATA_ZERO : STD_LOGIC;
-  -- IREG (instruction register) and instruction decoder
-  TYPE t_instr IS (halt, inc, dec, ptr_inc, ptr_dec, output, input, wloop_begin, wloop_end, dloop_begin, dloop_end);
-  SIGNAL IREG     : STD_LOGIC_VECTOR(15 DOWNTO 0);
-  SIGNAL IREG_DEC : t_instr;
   -- FSM (finite state machine)
-  TYPE t_state IS (start, fetch, decode, ex_inc, ex_dec, ex_ptr_inc, ex_ptr_dec, ex_output, ex_input, ex_wloop_begin, ex_wloop_end, ex_dloop_begin, ex_dloop_end, halt);
+  TYPE t_state IS (idle, fetch, decode, ex_ptr_inc, ex_ptr_dec, ex_val_inc, ex_val_dec, ex_print, ex_read, ex_wloop_beg, ex_wloop_end, ex_dloop_beg, ex_dloop_end, ex_noop, halt);
   SIGNAL PSTATE : t_state;
   SIGNAL NSTATE : t_state;
 BEGIN
+  -- PC (program counter)
+  pc : PROCESS (PC_INC, PC_DEC, RESET, CLK)
+  BEGIN
+    IF (RESET = '1') THEN
+      PC <= (OTHERS => '0');
+    ELSIF (rising_edge(CLK)) THEN
+      IF (PC_INC = '1') THEN
+        PC <= PC + 1;
+      ELSIF (PC_DEC = '1') THEN
+        PC <= PC - 1;
+      END IF;
+    END IF;
+  END PROCESS;
 
+  -- PTR (pointer to data in memory)
+  ptr : PROCESS (PTR_INC, PTR_DEC, RESET, CLK)
+  BEGIN
+    IF (RESET = '1') THEN
+      PTR <= (OTHERS => '0');
+    ELSIF (rising_edge(CLK)) THEN
+      IF (PTR_INC = '1') THEN
+        PTR <= PTR + 1;
+      ELSIF (PTR_DEC = '1') THEN
+        PTR <= PTR - 1;
+      END IF;
+    END IF;
+  END PROCESS;
+
+  -- CNT (counter for loops)
+  cnt : PROCESS (CNT_INC, CNT_DEC, RESET, CLK)
+  BEGIN
+    IF (RESET = '1') THEN
+      CNT      <= (OTHERS => '0');
+      CNT_ZERO <= '1';
+    ELSIF (rising_edge(CLK)) THEN
+      IF (CNT_INC = '1') THEN
+        CNT <= CNT + 1;
+      ELSIF (CNT_DEC = '1') THEN
+        CNT <= CNT - 1;
+      END IF;
+    END IF;
+  END PROCESS;
+  -- CNT_ZERO (log.1 if CNT == 0, else log.0)
+  cnt_zero : PROCESS (CNT, RESET, CLK)
+  BEGIN
+    IF (RESET = '1') THEN
+      CNT_ZERO <= '1';
+    ELSIF (rising_edge(CLK)) THEN
+      CASE (CNT = (OTHERS => '0'))
+        WHEN TRUE           => CNT_ZERO   <= '1';
+        WHEN OTHERS         => CNT_ZERO <= '0';
+      END CASE;
+    END IF;
+  END PROCESS;
+
+  -- MX1 (program or data address in memory)
+  mx1 : PROCESS (PC, PTR, MX1_SEL)
+  BEGIN
+    CASE MX1_SEL IS
+      WHEN '0'    => DATA_ADDR <= PC;
+      WHEN '1'    => DATA_ADDR <= PTR;
+      WHEN OTHERS => NULL;
+    END CASE;
+  END PROCESS;
+
+  -- MX2 (value to write to memory)
+  mx2 : PROCESS (IN_DATA, DATA_RDATA, MX2_SEL)
+  BEGIN
+    CASE MX2_SEL IS
+      WHEN "00"   => DATA_WDATA <= IN_DATA;
+      WHEN "01"   => DATA_WDATA <= DATA_RDATA;
+      WHEN "10"   => DATA_WDATA <= DATA_RDATA - 1;
+      WHEN "11"   => DATA_WDATA <= DATA_RDATA + 1;
+      WHEN OTHERS => NULL;
+    END CASE;
+  END PROCESS;
 END behavioral;
